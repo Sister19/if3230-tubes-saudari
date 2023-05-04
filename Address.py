0c2f69b6a9d09bcdbac3ec8be2f481996ac943fa
malikrafsan <pro.malikakbar2357@gmail.com>
2023-05-04 23:53:39 +0700

    feat: migrate using new msgs passing design

--- Address.py
+++ Address.py
@@ -1,3 +1,9 @@
+from typing import TypedDict
+
+class AddressData(TypedDict):
+    ip: str
+    port: int
+
 class Address(dict):
     def __init__(self, ip: str, port: int):
         dict.__init__(self, ip=ip, port=port)
@@ -10,8 +16,11 @@ class Address(dict):
     def __iter__(self):
         return iter((self.ip, self.port))
 
-    def __eq__(self, other):
+    def __eq__(self, other: "Address"):
         return self.ip == other.ip and self.port == other.port
 
-    def __ne__(self, other):
+    def __ne__(self, other: "Address"):
         return self.ip != other.ip or self.port != other.port
+
+    def data(self) -> AddressData:
+        return AddressData(ip=self.ip, port=self.port)
--- RaftNode.py
+++ RaftNode.py
@@ -6,8 +6,11 @@ from enum import Enum
 from Address import Address
 import socket
 import time
-import json
-
+from msgs.ApplyMembershipMsg import ApplyMembershipReq, ApplyMembershipResp
+from msgs.BaseMsg import BaseMsg
+from msgs.HeartbeatMsg import HeartbeatResp
+from msgs.ExecuteMsg import ExecuteReq, ExecuteResp
+from utils.MsgParser import MsgParser
 
 class RaftNode:
     HEARTBEAT_INTERVAL = 1
@@ -20,15 +23,19 @@ class RaftNode:
         CANDIDATE = 2
         FOLLOWER = 3
 
+    class FuncRPC(Enum):
+        APPLY_MEMBERSHIP = "apply_membership"
+
     def __init__(self, application: Any, addr: Address, contact_addr: Address = None):
         socket.setdefaulttimeout(RaftNode.RPC_TIMEOUT)
         self.address:             Address = addr
         self.type:                RaftNode.NodeType = RaftNode.NodeType.FOLLOWER
-        self.log:                 List[str, str] = []
+        self.log:                 List[str] = []
         self.app:                 Any = application
         self.election_term:       int = 0
         self.cluster_addr_list:   List[Address] = []
         self.cluster_leader_addr: Address = None
+        self.msg_parser: MsgParser = MsgParser()
         if contact_addr is None:
             self.cluster_addr_list.append(self.address)
             self.__initialize_as_leader()
@@ -61,43 +68,57 @@ class RaftNode:
 
     def __try_to_apply_membership(self, contact_addr: Address):
         redirected_addr = contact_addr
-        response = {
+        response = ApplyMembershipResp({
             "status": "redirected",
-            "address": {
-                "ip":   contact_addr.ip,
-                "port": contact_addr.port,
-            }
-        }
+            "address": contact_addr
+        })
         while response["status"] != "success":
             redirected_addr = Address(
                 response["address"]["ip"], response["address"]["port"])
-            response = self.__send_request(
-                self.address, "apply_membership", redirected_addr)
+            msg = ApplyMembershipReq(self.address)
+            print(f"Sending msg : {msg}")
+            response: ApplyMembershipResp = self.__send_request(
+                msg, RaftNode.FuncRPC.APPLY_MEMBERSHIP.value, redirected_addr)
         self.log = response["log"]
         self.cluster_addr_list = response["cluster_addr_list"]
         self.cluster_leader_addr = redirected_addr
 
-    def __send_request(self, request: Any, rpc_name: str, addr: Address) -> "json":
+    def __send_request(self, request: BaseMsg, rpc_name: str, addr: Address) -> BaseMsg:
         # Warning : This method is blocking
         node = ServerProxy(f"http://{addr.ip}:{addr.port}")
-        json_request = json.dumps(request)
+        json_request = self.msg_parser.serialize(request)
+        print(f"Sending request to {addr.ip}:{addr.port}...")
+        print(f"rpc_name : {rpc_name}")
         rpc_function = getattr(node, rpc_name)
-        response = json.loads(rpc_function(json_request))
+        print(f"RPC function : {rpc_function}")
+        response = self.msg_parser.deserialize(rpc_function(json_request))
         self.__print_log(response)
         return response
 
     # Inter-node RPCs
-    def heartbeat(self, json_request: str) -> "json":
+    def heartbeat(self, json_request: str) -> str:
         # TODO : Implement heartbeat
-        response = {
+        response = HeartbeatResp({
             "heartbeat_response": "ack",
             "address":            self.address,
-        }
-        return json.dumps(response)
+        })
+        return self.msg_parser.serialize(response)
 
     # Client RPCs
+    def apply_membership(self, json_request: str) -> str:
+        # TODO : Implement apply_membership
+        request = self.msg_parser.deserialize(json_request)
+        print(f"Request : {request}")
+        self.cluster_addr_list.append(Address(request["ip"], request["port"]))
+        response = ApplyMembershipResp({
+            "status":            "success",
+            "log":               self.log,
+            "cluster_addr_list": self.cluster_addr_list,
+        })
+        return self.msg_parser.serialize(response)
 
-    def execute(self, json_request: str) -> "json":
-        request = json.loads(json_request)
+    def execute(self, json_request: str) -> str:
+        request: ExecuteReq = self.msg_parser.deserialize(json_request)
         # TODO : Implement execute
-        return json.dumps(request)
+        response = ExecuteResp({})
+        return self.msg_parser.serialize(response)
