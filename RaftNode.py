7ef6d0e3839c33b71ae276a8acf91f9014fea1b1
Rifqi Naufal Abdjul <rifqi.abdjul23@gmail.com>
2023-05-14 00:03:41 +0700

    chores: adding fixmes
    tebak

--- RaftNode.py
+++ RaftNode.py
@@ -15,6 +15,7 @@ from structs.Log import Log
 from msgs.VoteMsg import VoteReq, VoteResp
 
 class RaftNode:
+    # FIXME: knp di dalem class? mending taro luar biar bisa dipake
     HEARTBEAT_INTERVAL = 1
     ELECTION_TIMEOUT_MIN = 2
     ELECTION_TIMEOUT_MAX = 3
@@ -29,6 +30,8 @@ class RaftNode:
         APPLY_MEMBERSHIP = "apply_membership"
         VOTE_LEADER = "vote_leader"
 
+    # Heeh, semua yang diatas dari komen ini
+
     def __init__(self, application: Any, addr: Address, contact_addr: Address = None):
         socket.setdefaulttimeout(RaftNode.RPC_TIMEOUT)
         self.address:             Address = addr
@@ -41,7 +44,7 @@ class RaftNode:
         self.__init_volatile()
 
         # additional vars
-        self.cluster_addr_list:   List[Address] = []
+        self.cluster_addr_list:   List[Address] = [] # FIXME: Lebih baik dijadiin struct ajah, daripada bergantung dengan index, karena di join sama data last sent
         self.msg_parser: MsgParser = MsgParser()
 
         if contact_addr is None:
@@ -62,32 +65,34 @@ class RaftNode:
 
     def __init_stable(self):
         self.election_term:       int = 0
-        self.voted_for: Address     = None # TODO: add type
+        self.voted_for: Address     = None # TODO: add type, #QUESTIONABLE: hah ngapain dia voted ke selain candidate?
         self.log:                 List[Log] = []
         self.commit_length: int = 0
 
     def __init_volatile(self):
         self.type:                RaftNode.NodeType = RaftNode.NodeType.FOLLOWER
         self.cluster_leader_addr: Address = None
-        self.votes_received: List[Address] = []
-        self.sent_length = []
+        self.votes_received: List[Address] = [] # FIXME: Ini mah gausah disimpen, sementara
+        self.sent_length = [] # FIXME: Sent length gaperlu, ini bukan tcp wkwk
         self.acked_length = []
 
     def vote_leader(self):
         self.election_term += 1
         self.type = RaftNode.NodeType.CANDIDATE
-        self.voted_for = self.address
+        self.voted_for = self.address # FIXME: voted_for = leader_addr? ato beda? bisa disamain kok
         self.votes_received = [self.address]
         
         last_term = 0
         if len(self.log) > 0:
             last_term = self.log[-1]["term"]
+
+        # FIXME: Check konsistensi log juga
         
         msg = VoteReq({
             'voted_for': self.voted_for, 
             'term': self.election_term,
             'log_length': len(self.log),
-            'last_term': last_term,
+            'last_term': last_term, # FIXME: Last term buat apa?
         })
 
         for i in range(len(self.cluster_addr_list)):
@@ -96,7 +101,7 @@ class RaftNode:
                 addr, msg
             )
             # TODO: start timer
-        
+        # TODO: pindahin start timer ke sini
 
     def __try_request_vote(self, addr_dest: Address, msg: VoteReq):
         redirected_addr = addr_dest
@@ -105,7 +110,7 @@ class RaftNode:
             "address": addr_dest,
         })
 
-        while response["status"] != "success":
+        while response["status"] != "success": # FIXME: Recursive redirected gini seharusnya di bikin wrapper aja biar bisa dipake lagi
             redirected_addr = Address(
                 response["address"]["ip"],
                 response["address"]["port"],
@@ -121,18 +126,18 @@ class RaftNode:
 
     # Internal Raft Node methods
 
-    def __print_log(self, text: str):
+    def __print_log(self, text: str): # FIXME: Plis jangan print_log gua kira ngeprint isi log
         print(f"[{self.address}] [{time.strftime('%H:%M:%S')}] {text}")
 
     def __initialize_as_leader(self):
         self.__print_log("Initialize as leader node...")
-        self.cluster_leader_addr = self.address
+        self.cluster_leader_addr = self.address # FIXME: cluster_leader_addr = voted_for? ato beda? bisa disamain kok
         self.type = RaftNode.NodeType.LEADER
         request = {
             "cluster_leader_addr": self.address
         }
         # TODO : Inform to all node this is new leader
-        self.heartbeat_thread = Thread(target=asyncio.run, args=[
+        self.heartbeat_thread = Thread(target=asyncio.run, args=[ #FIXME: berarti klo bukan leader, thread loopnya beda lagi?
                                        self.__leader_heartbeat()])
         self.heartbeat_thread.start()
 
@@ -140,7 +145,7 @@ class RaftNode:
         # TODO : Send periodic heartbeat
         while True:
             self.__print_log("[Leader] Sending heartbeat...")
-            pass
+            pass # FIXME: WHaat?
             await asyncio.sleep(RaftNode.HEARTBEAT_INTERVAL)
 
     def __try_to_apply_membership(self, contact_addr: Address):
@@ -174,7 +179,7 @@ class RaftNode:
 
     # Inter-node RPCs
     def heartbeat(self, json_request: str) -> str:
-        # TODO : Implement heartbeat
+        # TODO : Implement heartbeat, reappend log baru dan check commitnya juga
         response = HeartbeatResp({
             "heartbeat_response": "ack",
             "address":            self.address,
@@ -182,14 +187,14 @@ class RaftNode:
         return self.msg_parser.serialize(response)
 
     # Client RPCs
-    def apply_membership(self, json_request: str) -> str:
+    def apply_membership(self, json_request: str) -> str: #FIXME: apply membership pake consensus, dan blocking 1 persatu
         # TODO : Implement apply_membership
         request = self.msg_parser.deserialize(json_request)
         print(f"Request : {request}")
         self.cluster_addr_list.append(Address(request["ip"], request["port"]))
         response = ApplyMembershipResp({
             "status":            "success",
-            "log":               self.log,
+            "log":               self.log, # FIXME: log terbaru apa semua? yang terbaru aja
             "cluster_addr_list": self.cluster_addr_list,
         })
         return self.msg_parser.serialize(response)
