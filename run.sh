69d8574ceb69f78aaf01e13b742d543eb81de498
malikrafsan <pro.malikakbar2357@gmail.com>
2023-05-21 12:54:17 +0700

    feat: update script to run http server and react client

--- run.sh
+++ run.sh
@@ -3,3 +3,6 @@
 gnome-terminal --tab --title='server1' -e "bash -c 'python3 server.py localhost 5000'";
 gnome-terminal --tab --title='server2' -e "bash -c 'python3 server.py localhost 5001 localhost 5000'";
 gnome-terminal --tab --title='server3' -e "bash -c 'python3 server.py localhost 5002 localhost 5000'"
+
+gnome-terminal --tab --title='http_server' -e "bash -c 'python3 http_server.py'"
+gnome-terminal --tab --title='client' -e "bash -c 'cd raft-dashboard && npm run dev'"
